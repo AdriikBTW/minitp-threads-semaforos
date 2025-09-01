ed5129f1fc87be49397be44518e423ad95bfd0ad
nahuelcarrizo9 <carrizonahue86@gmail.com>
2025-09-01 23:44:13 +0000

    Se agregaron librerías, se lanzan los threads y se define la estructura basica

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <semaphore.h>
 
 // TODO: implementar
 
@@ -13,9 +15,35 @@ void descenderATierra();
 void abrirCompuertas();
 void abandonarTierra();
 
-int main(void)
-{
+void* presidentes(void* arg){
+        //Implementar
+}
+void* naves_destructoras(void* arg){
+        //Implementar
+}
+void* ciudades(void* arg){
+        //Implementar
+}
+void* comandante(void* arg){
+        //Implementar
+}
+int main(void){
         printf("Hello there!\n");
+        pthread_t presidentes[5];
+        pthread_t naves_destructoras[33];
+        pthread_t ciudades[33];
+        pthread_t comandante;
+
+        for(int i=0;i<5;i++){
+                pthread_create(&presidentes[i],NULL,presidentes,NULL);
+        }
+        for(int i=0;i<33;i++){
+                pthread_create(&naves_destructoras[i],NULL,naves_destructoras,NULL);
+        }
+        for(int i=0;i<33;i++){
+                pthread_create(&ciudades[i],NULL,ciudades,NULL);
+        }
+        pthread_create(comandante,NULL,comandante,NULL);
 
         return 0;
 }
