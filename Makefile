75f7a01f1d2924f0a5437cc7a1bf905fb94b899d
Adriik <adriik.linux@proton.me>
2025-09-01 10:33:06 -0300

    chore: initial commit
    Signed-off-by: Adriik <adriik.linux@proton.me>

---
+++ Makefile
@@ -0,0 +1,23 @@
+CC = gcc
+CFLAGS = -Wall -Wextra -std=c99 -O2
+
+# Target binary
+TARGET = build/main
+
+# Source file
+SRC = main.c
+
+# Default target
+all: $(TARGET)
+
+# Rule to build the binary
+$(TARGET): $(SRC) | build
+	$(CC) $(CFLAGS) -o $@ $<
+
+# Ensure build directory exists
+build:
+	mkdir -p build
+
+# Clean up
+clean:
+	rm -rf build
---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+// TODO: implementar
+
+void hablarConComandanteExtraterrestre();
+void presentarseALideresMundiales();
+void tomarPosicionEnMegaCiudad();
+void atacarMegaciudad();
+void iniciarPotocoloDefensivo();
+void contraatacarNaves();
+void perderBatalla();
+void descenderATierra();
+void abrirCompuertas();
+void abandonarTierra();
+
+int main(void)
+{
+        printf("Hello there!\n");
+
+        return 0;
+}
